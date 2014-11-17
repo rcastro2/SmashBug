dbc46c7c3e1c517fdd7def9d26083950a79bf813
rcastro2 <rcastro@baysidehighschool.org>
2014-11-16 20:17:45 -0500

    Added score object and variable

--- GameScreen.js
+++ GameScreen.js
@@ -1,13 +1,15 @@
 GameScreen = function(width,height)
 {
     GameScreen.superclass.constructor.apply(this,arguments);
-	this.background = new Sprite(this,"grass")
-	this.spider = new Anim(this,"spider",5,4,19,true)
+	this.background = new Sprite(this,"grass");
+	this.spider = new Anim(this,"spider",5,4,19,true);
+	this.scoreText = new Text(this);
+	this.score = 0;
 };
 GameScreen.prototype =
 {	
 	updateScreen:function(event){
-		this.spider.move()
+		this.spider.move();
 	},
 	click: function(event){
 
