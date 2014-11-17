16537502ad171c13dead1874a3d3b7bd2c64c90c
rcastro2 <rcastro@baysidehighschool.org>
2014-11-16 19:26:34 -0500

    Added animated spider

--- GameScreen.js
+++ GameScreen.js
@@ -2,7 +2,7 @@ GameScreen = function(width,height)
 {
     GameScreen.superclass.constructor.apply(this,arguments);
 	this.background = new Sprite(this,"grass")
-
+	this.spider = new Anim(this,"spider",5,4,19,true)
 };
 GameScreen.prototype =
 {	
